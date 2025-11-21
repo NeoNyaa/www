f1607794d71ccd496261f8c5d5e56ca269eb7da4
NeoNyaa <56982408+NeoNyaa@users.noreply.github.com>
2025-11-21 23:19:27 +0000

    applied prettier formating to all files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 </svelte:head>
 
 <nav>
-	<a href="/" aria-label="home"><img src="{favicon}" alt="Site favicon" /></a>
+	<a href="/" aria-label="home"><img src={favicon} alt="Site favicon" /></a>
 	<a href="/">Home</a>
 	<a href="/links">Links</a>
 	<a href="/utils/hypixel-skyblock/Experiment-Book-Meter">Experiment-Book-Meter</a>
