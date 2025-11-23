93fceccccea38a87995668d4f9f7c96240b1cec3
NeoNyaa <56982408+NeoNyaa@users.noreply.github.com>
2025-11-23 19:59:12 +0000

    Let's do this properly this time

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/img/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav class="bg-background-100">
+	<a href="/" aria-label="home"><img src={favicon} alt="Site favicon" /></a>
+	<a href="/links">Links</a>
+	<a href="/utils/hypixel-skyblock/Experiment-Book-Meter">Experiment-Book-Meter</a>
+	<pre class="text-red-500"> -[ IN DEVELOPMENT ]- </pre>
+</nav>
+
+{@render children()}
